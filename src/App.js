2565a835882b672518ec8f0f96834c6b893f7c07
luc-nguyen-174 <louiskingvn@gmail.com>
2023-05-08 00:38:29 +0700

    update nav, add component login, header, product

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Products from './components/Products';
 import Product from './components/Product';
-import Footer from './components/Footer';
-import {Route, Routes} from 'react-router-dom';
-import Carousel from "bootstrap/js/src/carousel";
-import Category from "./components/Category";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LoginForm";
@@ -14,13 +10,15 @@ function App() {
     return (
         <BrowserRouter>
             <Navbar/>
+
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/products" element={<Products/>}/>
-                <Route path="/product/:id" element={<Product/>}/>
+                <Route path="/products" element={<Home/>}/>
+                <Route path="/products/:id" element={<Product/>}/>
                 <Route path="/signup" element={<SignUpForm/>}/>
                 <Route path="/login" element={<LoginForm/>}/>
             </Routes>
+
         </BrowserRouter>
     );
 }
